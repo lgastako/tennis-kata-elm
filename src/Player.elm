d1240691f38154fcf1df26acd2473cc674a7d1aa
John Evans <lgastako@gmail.com>
2016-11-02 00:30:21 -0600

    Simplify Player.toString.

--- src/Player.elm
+++ src/Player.elm
@@ -1,7 +1,5 @@
 module Player exposing (Player(Player1, Player2), toString)
 
-import Regex exposing (HowMany(All), regex)
-
 
 type Player
     = Player1
@@ -10,6 +8,9 @@ type Player
 
 toString : Player -> String
 toString player =
-    player
-        |> Basics.toString
-        |> Regex.replace All (regex "\\d+") (\{ match } -> " " ++ match)
+    case player of
+        Player1 ->
+            "Player 1"
+
+        Player2 ->
+            "Player 2"
